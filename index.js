6a90eea07247b648756679b9abf30ea425d96e17
Travis Tokuyama <travistokuyama@gmail.com>
2018-07-18 01:59:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// add solution here
+function theBeatlesPlay(musician, instrument) {
+  var whoPlaysWhat = [];
+  for(var i=0; i<musician.length; i++) {
+    whoPlaysWhat.push(`${musician[i]} plays ${instrument[i]}`)
+  }
+  return whoPlaysWhat
+}
+
+function johnLennonFacts(array) {
+  
+}
